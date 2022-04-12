a28ff998c6d3908d963070c204691c474c50b4be
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-04-12 14:26:46 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("GE2A_12_ハヤシ_タカユキ");
+	win->CreateGameWindow("GE2A_12_ハヤシ_タカユキ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
